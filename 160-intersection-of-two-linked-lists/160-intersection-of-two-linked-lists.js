f341255e4a9f16202cf41b5d685aae9d4dbf1d0e
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-07-04 18:20:31 +0200

    Time: 133 ms (51.64%), Space: 50.3 MB (40.50%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.js
@@ -12,17 +12,21 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let vals = [];
-    
-    while (headA) {
-        vals.push(headA);
-        headA = headA.next;
-    }
+    let firstPointer = headA;
+    let secondPointer = headB;
 
-    while (headB) {
-        if (vals.includes(headB)) return headB;
-        headB = headB.next;
+    while(firstPointer != secondPointer) {
+        /*each pointer will progress forward one index value at a time equally. 
+		If one list is longer than the other,or skip condition, pointer on shorter list will switch to the head of the other list,
+		eventually intersecting with each other. If the lists to not intersect, they will intersect at null, resulting in return null */
+        //ternary operator (?), replaces if else statement
+        //while condition ? if true, firstPointer.next : else false, headB
+        firstPointer = firstPointer ? firstPointer.next : headB;
+        //while condition ? if true, secondPointer.next : else false, headA
+        secondPointer = secondPointer ? secondPointer.next : headA;
+        // console.log('firstPointer[i]:', firstPointer);
+        // console.log('secondPointer[i]:', secondPointer);
     }
-    return null;
+    return firstPointer;
     
 };
