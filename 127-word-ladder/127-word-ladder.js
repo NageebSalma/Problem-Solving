e38744941bb2f5b16edf7268ed0389a33ee186e6
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-07-09 23:51:35 +0200

    Time: 373 ms (53.51%), Space: 50.7 MB (57.66%) - LeetHub

--- 127-word-ladder/127-word-ladder.js
+++ 127-word-ladder/127-word-ladder.js
@@ -5,39 +5,70 @@
  * @return {number}
  */
 var ladderLength = function(beginWord, endWord, wordList) {
-    let hash = {};
+  // Because 'wordList' is an array, lookups for values
+  // will be a linear search ( O(n) ), so we'll convert
+  // it to a Set so we can get O(1) lookup time for words
+  const wordsSet = new Set(wordList)
 
-    for(let word of wordList){
-       if(word!== beginWord) hash[word] = true;
-    }
-    if(!hash[endWord]) return 0;
-    
-    
-    let queue = [beginWord];
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    let level = 1;
-    
-    while(queue.length){
-        let next = []
-        
-        for(let word of queue){
-            
-            if(word=== endWord) return level;
+  // Check if our endWord is in our set
+  if (!wordsSet.has(endWord)) return 0
 
-            for(let i= 0 ; i < word.length;  i++){
-                for(let letter of alphabet){
-                    let modified = word.slice(0,i)+letter+word.slice(i+1)
-                    if(hash[modified]){
-                        next.push(modified)
-                        delete hash[modified]
-                    }
-                }
-            }
+  // Initialize our queue to support BFS
+  // NOTE: Arrays are not the best data structure to
+  // model a queue, however, it is built into JS and
+  // works well enough for this example. There is a
+  // potencial for performance gains by replacing this
+  // with a custom SLL implementation.
+  const queue = []
+  queue.push(beginWord)
+
+  // Initialize our Set to keep track of words we've seen
+  const seenWords = new Set()
+  seenWords.add(beginWord)
+
+  // Because we know there is a path to our end word we'll
+  // initialize our changes counter to 1
+  let changes = 1
+
+  while (queue.length !== 0) {
+    // Keep track of our queue size, when we've looped through
+    // the current size, we'll increment our 'changes' counter
+    // and then resume working the queue
+    const size = queue.length
+    for (let i = 0; i < size; i++) {
+      // Pull the first word out of our queue
+      const word = queue.shift()
+
+      // Check if we've found our end word
+      if (word === endWord) return changes
+
+      // Loop over all chars in the word, changing them, to
+      // see if we can construct new words that are in our
+      // word list
+      for (let i = 0; i < word.length; i++) {
+        // Replace each char with a-z using unicode (97=a, 122=z)
+        for (let j = 97; j < 123; j++) {
+          const newWord = (
+            word.substring(0, i) + String.fromCharCode(j)
+          ).concat(word.substring(i + 1))
+
+          // Check if we found a new word that is in our list, but
+          // hasn't been encountered before
+          if (wordsSet.has(newWord) && !seenWords.has(newWord)) {
+            // It's a new word, track it in our sets
+            queue.push(newWord)
+            seenWords.add(newWord)
+          }
         }
-        queue = next;
-        level++;
-        
+      }
     }
-    return 0;
+
+    // Increment our changes counter
+    ++changes
+  }
+
+  // We were unable to find a path to our 'endWord', so we'll return
+  // 0 here
+  return 0
 };
 
