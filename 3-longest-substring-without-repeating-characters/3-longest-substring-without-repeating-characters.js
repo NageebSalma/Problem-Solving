cb7c5a83f40d58186d433d698969ca7c1c4573db
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-07-14 00:07:26 +0200

    Time: 129 ms (66.56%), Space: 47 MB (69.45%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,92 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+
+// p w w k e w
+//     i
+//       j
+
+// sub        | unique? | max_ans
+// p            yes        1
+// pw           yes        2
+// pww          no         2
+// w            yes        2
+// ww           no         2
+// w            yes        2
+// wk           yes        2
+// wke          yes        3
+// wkew         no         3
+
+
+
+//brute force
+// let isUnique = (str) => {
+//     var hashtable = {};
+//     for(var i=0,len=str.length;i<len;i++){
+//         if (hashtable[str[i]] != null){
+//             hashtable[str[i]] = 1;
+//             return false;
+//         }else{
+//             hashtable[str[i]] = 0;
+//         }
+//     }
+//     return true;
+
+// }
+// var lengthOfLongestSubstring = function(s) {
+//     let substring = ''
+//     let ans = 0;
+//     for(let i = 0  ; i < s.length ; i++){
+//        for(let j  = 0 ; j < s.length ; j++){
+//             substring = s.substring(i , j+1)
+//             if(isUnique(substring)){
+//                 ans = Math.max(ans , substring.length)
+//             }
+//           }
+//     }
+//     return ans;
+// };
+
+
+
+
+
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let ans = 0;
+    let i = 0 ; 
+    let j = 0;
+    
+    while(j <= i && i < s.length) {
+        if(!set.has(s[i])){
+            set.add(s[i]);
+              let check = i - j +1
+            i++;
+          
+            ans = Math.max(ans , check);
+            // console.log(set)
+            // console.log(ans)
+        }
+        else{
+            if(set.has(s[j])){
+                set.delete(s[j]);
+                j++;
+            }
+        }
+        
+        
+    }
+    return ans
+};
+
+
+
+
+
+
+
+
+
+
