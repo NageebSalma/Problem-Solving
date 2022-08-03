fea4d476954ccfa882bb4a2565a2caa012ed38e6
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-08-03 19:26:44 +0200

    Time: 120 ms (62.02%), Space: 49.4 MB (43.73%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -12,19 +12,30 @@
  * @return {boolean}
  */
 var isSubtree = function(root, subRoot) {    
-    const isSame = (n1, n2) => {
-        if (!n1 && !n2) return true;
-        if (!n1 || !n2 || n1.val !== n2.val) return false;
-        return isSame(n1.left, n2.left) && isSame(n1.right, n2.right);
-    };
-    
-       // check if subRoot is subtree of root:
-    const DFS = (node) => {
-        if (!node) return false;
-        if (isSame(node, subRoot)) return true;
-        return DFS(node.left) || DFS(node.right);
-    };
+    if(subRoot == null) return true;
+    else if(root == null) return false
+    if(isSameTree(root,subRoot)) return true;
+    else return (isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot))
+}
 
-    return DFS(root);
-   
+var isSameTree = function(p, q) {
+    if(p==null && q==null) return true;
+    else if(p==null || q==null) return false;
+    
+    if(p.val == q.val) return isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
+    else return false
 };
+//  const isSame = (n1, n2) => {
+//         if (!n1 && !n2) return true;
+//         if (!n1 || !n2 || n1.val !== n2.val) return false;
+//         return isSame(n1.left, n2.left) && isSame(n1.right, n2.right);
+//     };
+    
+//        // check if subRoot is subtree of root:
+//     const DFS = (node) => {
+//         if (!node) return false;
+//         if (isSame(node, subRoot)) return true;
+//         return DFS(node.left) || DFS(node.right);
+//     };
+
+//     return DFS(root);
