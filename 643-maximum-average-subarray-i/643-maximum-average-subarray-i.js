4644b61753c1789eb5c594556a7699e8517a8532
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-08-09 19:33:03 +0200

    Time: 167 ms (28.91%), Space: 55 MB (37.38%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
@@ -3,17 +3,27 @@
  * @param {number} k
  * @return {number}
  */
+
+//[1,12,-5,-6,50,3], k = 4
+//___________
+//sum = 1 + 12 + -5 + -6
+
 var findMaxAverage = function(nums, k) {
+    // let tracker = [];
     let output = Number.NEGATIVE_INFINITY;
     let sum = 0;
+    let windowStart= 0;
     
-    for(let i = 0 ; i < nums.length-k+1 ; i++){
-        for(let j = i ; j < k+i ; j++){
-            sum += nums[j];
+    for(let  windowEnd = 0; windowEnd < nums.length; windowEnd++){
+        sum += nums[windowEnd]
+        if(windowEnd >= k-1){
+            // tracker.push(sum/k);
+            output = Math.max(output , sum/k)
+
+            sum = sum - nums[windowStart];
+            windowStart++;
         }
-        output = Math.max(sum/k , output)
-        sum = 0;
     }
-    
+    // console.log(tracker);
     return output;
 };
