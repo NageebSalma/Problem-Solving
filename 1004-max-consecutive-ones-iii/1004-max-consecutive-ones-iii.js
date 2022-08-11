dafc247d23ab441358b1db63ae67da0a5ea85f02
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-08-11 15:43:27 +0200

    Time: 146 ms (13.77%), Space: 47.5 MB (15.47%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var longestOnes = function(str, k) {
-    let tracker = {1:0 , 0 : 0},
+    let tracker = {1: 0 , 0 : 0},
       max_len = 0,
       window_start = 0,
       window_end = 0,
@@ -13,7 +13,7 @@ var longestOnes = function(str, k) {
   for(window_end ; window_end < str.length ; window_end++){
     tracker[str[window_end]] ? tracker[str[window_end]]++ :  tracker[str[window_end]] = 1;
 
-    if(window_end - window_start + 1 - tracker[1] > k){
+    while(window_end - window_start + 1 - tracker[1] > k){
       tracker[str[window_start]]--;
       if(tracker[str[window_start]] <= 0)tracker[str[window_start]] = 0
       window_start++;
