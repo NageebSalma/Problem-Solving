47bbe4b5525711758564703db51bdee8b4dfff2c
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-08-18 21:23:23 +0200

    Time: 184 ms (79.96%), Space: 69.7 MB (78.10%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.js
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -10,13 +10,39 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-  let reversed = '';
-  let forward = ''
-  while(head){
-    reversed = head.val+reversed;
-    forward += head.val;
-    head = head.next;
+  let mid = find_mid_of_linkedlist(head);
+    
+  let pointer1 = head;
+  let pointer2 = mid;
+  pointer2 = reverse_linked_list(pointer2);
+    
+  while(pointer2 !== null){
+      if(pointer1.val !== pointer2.val) return false;
+      pointer1 = pointer1.next;
+      pointer2 = pointer2.next;
   }
-  
-  return forward !== reversed ? false : true;
-};
+    return true;
+}
+
+function find_mid_of_linkedlist(head){
+  let fast = head , slow = head;
+  while(fast !== null && fast.next !== null){
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return fast!== null ? slow.next : slow;
+}
+
+function reverse_linked_list(head){
+    let prev = null;
+    let current = head;
+    let tracker 
+    while(current){
+        tracker = current.next;
+        current.next = prev;
+        prev = current 
+        current = tracker;
+    }
+    return prev;
+}
