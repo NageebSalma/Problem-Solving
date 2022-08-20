0ca0d7870253591c20edb88aad3ac7db58023d73
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-08-20 13:49:03 +0200

    Time: 191 ms (11.80%), Space: 48.1 MB (93.84%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.js
+++ 986-interval-list-intersections/986-interval-list-intersections.js
@@ -4,20 +4,28 @@
  * @return {number[][]}
  */
 var intervalIntersection = function(aList, bList) {
-    let result = [],
-    i = 0,
-    j = 0;
-
-  while (i < aList.length && j < bList.length) {
-      let a_overlaps_b = aList[i][0] >= bList[j][0] && aList[i][0] <= bList[j][1];
-      let b_overlaps_a = bList[j][0] >= aList[i][0] && bList[j][0] <= aList[i][1];
-      
-      if(a_overlaps_b || b_overlaps_a){
-          result.push([Math.max(aList[i][0] , bList[j][0]) , Math.min(aList[i][1] , bList[j][1])]);
-      }
-     
-      if(aList[i][1] < bList[j][1]) i++;
-      else j++;
-  }
+    let result = [], i = 0, j = 0;
+    
+    while(i < aList.length && j < bList.length){
+        let a_intersects_b = aList[i][0] <= bList[j][0] && aList[i][1] >= bList[j][0];
+        let b_intersects_a = bList[j][0] <= aList[i][0] && bList[j][1] >= aList[i][0];
+        
+        if(a_intersects_b || b_intersects_a){
+            result.push([
+                Math.max(aList[i][0] , bList[j][0])
+                ,
+                Math.min(bList[j][1] , aList[i][1])
+            ]);
+        }
+        
+        if(aList[i][1] < bList[j][1]) i++;
+        else j++;
+        
+    }
+    
     return result;
 };
+
+
+//when does a intersect b? a.start <= b.start && a.end >= b.start
+//when does b intersect a? b.start <= a.start && b.end >= a.start
