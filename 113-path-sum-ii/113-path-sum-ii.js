f9dd32ebbf987e9df2f47e326ca0f51ff4001ca9
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-09-15 20:25:47 +0200

    Time: 140 ms (19.68%), Space: 56.5 MB (10.33%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function(root, targetSum) {
+    let output = []
+    
+   path_sum_helper(root, targetSum , [] , output)
+    
+    return output;
+};
+
+function path_sum_helper(root, targetSum , currentPath , output){
+    if(!root) return;
+    
+    currentPath.push(root.val);
+    
+    if(root.val == targetSum && !root.left && !root.right) output.push(currentPath)
+    
+    else{
+        path_sum_helper(root.left , targetSum - root.val , currentPath.slice(0) , output);
+        path_sum_helper(root.right, targetSum - root.val , currentPath.slice(0) , output);
+    }
+    
+}
