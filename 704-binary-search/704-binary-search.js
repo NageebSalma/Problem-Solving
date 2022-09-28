18f9ee0d80678ac962cc54a2c817189ff10ff09d
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-09-29 00:14:13 +0200

    Time: 55 ms (99.70%), Space: 45 MB (68.81%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    if(!nums.length) return -1;
+    if(nums.length==1) return nums[0] == target? 0 : -1;
+    
+    let left=0, right=nums.length-1;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] == target) return mid;
+        
+        if(target > nums[mid]) left = mid+1;
+        else right = mid-1;
+    };
+    
+    return -1;
+};
