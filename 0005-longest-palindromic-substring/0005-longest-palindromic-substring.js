be71a9b7f15f8098f29b21b76403cfce69a8fc17
Salma Nageeb <59023057+NageebSalma@users.noreply.github.com>
2022-10-10 16:09:44 +0200

    Time: 142 ms (69.89%), Space: 44.7 MB (72.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -7,32 +7,27 @@ var longestPalindrome = function(s) {
     
     let res = "", output =""
     let max_length = 0;
-    let oddLength = false;
-    if(!(s.length % 2)) oddLength = true;
     
     for(let i = 0 ; i < s.length ; i++){
-        let l= i, r=i;
         
+        let l= i, r=i;
         while(l >= 0 && r < s.length && s[l] == s[r]){
             if(r-l+1 > max_length){
                 res = s.slice(l,r+1);
                 output = res; 
                 max_length = res.length;
             };
-            l--;
-            r++;
+            l--; r++;
         };
         
         l= i, r=i+1;
-        
         while(l >= 0 && r < s.length && s[l] == s[r]){
             if(r-l+1 > max_length){
                 res = s.slice(l,r+1);
                 output = res; 
                 max_length = res.length;
             };
-            l--;
-            r++;
+            l--; r++;
         };
     };
     return output; 
